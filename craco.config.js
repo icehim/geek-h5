7e4a8d4d44ba6d3a6e8a58ce76a801c2262ac3bb
zhiming <zhiming.hao@qq.com>
2022-12-23 18:39:31 +0800

    移动端1px像素边框问题

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,11 @@
 const path = require('path')
+//配置px转换vw=>移动端适配
+const pxToViewport = require('postcss-px-to-viewport')
+const vw = pxToViewport({
+    // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+    viewportWidth: 375
+})
+
 
 module.exports = {
     // webpack 配置
@@ -10,6 +17,23 @@ module.exports = {
             // 约定：使用 @scss 表示全局 SASS 样式所在路径
             // 在 SASS 中使用
             '@scss': path.resolve(__dirname, 'src/styles')
+        }
+    },
+    // 适配
+    // style: {
+    //   postcss: {
+    //     plugins: [vw]
+    //   }
+    // }
+    style: {
+        postcss: {
+            mode: 'extends',
+            loaderOptions: {
+                postcssOptions: {
+                    ident: 'postcss',
+                    plugins: [vw]
+                }
+            }
         }
     }
 }
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,14 +1,19 @@
 import {Button} from "antd-mobile";
-import './index.css'
+import './index.scss'
 
 function Test() {
     return (
         <div>
-            <div className='box'>
-                测试原生css变量
-            </div>
+            {/*测试原生css变量*/}
+            <div className='box'>测试原生css变量</div>
+            <hr/>
+            {/*1px像素边框问题*/}
+            <div className='bd'>1px像素边框问题</div>
+            <div className='btop'>1px像素边框问题</div>
+
+            {/*测试组件库是否引入成功*/}
+            <hr/>
             <h1>
-                测试
                 <Button color='success'>Success</Button>
                 <Button color='danger'>Danger</Button>
                 <Button color='warning'>Warning</Button>
