01c5f207d3a1703c24389039fd5e01381c19d982
zhiming <zhiming.hao@qq.com>
2022-12-23 20:23:12 +0800

    封装icon组件

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,26 @@
+/*
+* 图标组件:
+* 1.type=》图标类名
+* 2.className=》类名
+* 3.onClick=>回调函数
+* */
+//定义接受参数的类型
+import cla from 'classnames'
+
+type Props = {
+    type: string
+    className?: string
+    onClick?: () => void
+}
+
+
+const Icon = ({type, className, onClick}: Props) => {
+    return (
+        <svg onClick={onClick} className={cla('icon', className)} aria-hidden="true">
+            {/* 使用时，只需要将此处的 icon btn_like_sel 替换为 icon 的名称即可*/}
+            <use xlinkHref={`#${type}`}></use>
+        </svg>
+    );
+}
+
+export default Icon;
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,19 +1,21 @@
 import {Button} from "antd-mobile";
 import './index.scss'
 
+//导入图标组件
+import Icon from '@/components/icon'
+
 function Test() {
     return (
         <div>
             {/*测试图标*/}
-            <svg className="icon" aria-hidden="true">
-                {/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
-                <use xlinkHref="#iconbtn_like_sel"></use>
-            </svg>
-            <svg className="icon" aria-hidden="true">
-                {/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
-                <use xlinkHref="#iconbtn_collect"></use>
-            </svg>
+            {/*<svg className="icon" aria-hidden="true">*/}
+            {/*    /!* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*!/*/}
+            {/*    <use xlinkHref="#iconbtn_collect"></use>*/}
+            {/*</svg>*/}
 
+            <Icon type='iconbtn_like_sel' onClick={() => {
+                console.log(123)
+            }}/>
             {/*测试原生css变量*/}
             <div className='box'>测试原生css变量</div>
             <hr/>
