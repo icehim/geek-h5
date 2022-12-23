5841d7574b5fd94ee56f5d40974a42647dbcc68c
zhiming <zhiming.hao@qq.com>
2022-12-23 17:54:22 +0800

    配置路径别名'@'

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    // webpack 配置
+    webpack: {
+        // 配置别名
+        alias: {
+            // 约定：使用 @ 表示 src 文件所在路径
+            '@': path.resolve(__dirname, 'src'),
+            // 约定：使用 @scss 表示全局 SASS 样式所在路径
+            // 在 SASS 中使用
+            '@scss': path.resolve(__dirname, 'src/styles')
+        }
+    }
+}
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,14 +1,18 @@
 import {Button} from "antd-mobile";
+import './index.css'
 
 function Test() {
     return (
         <div>
+            <div className='box'>
+                测试原生css变量
+            </div>
             <h1>
                 测试
-                <Button loading loadingText='加载中' color='primary'>Primary</Button>
                 <Button color='success'>Success</Button>
                 <Button color='danger'>Danger</Button>
                 <Button color='warning'>Warning</Button>
+                <Button loading loadingText='加载中' color='primary'>Primary</Button>
             </h1>
         </div>
     );
