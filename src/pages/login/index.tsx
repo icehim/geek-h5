b1e4efbe0e0bd9395013ac02b4e94ffd28ae1098
zhiming <zhiming.hao@qq.com>
2022-12-27 11:52:07 +0800

    登录表单页面结构

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,42 @@
-import {Button} from "antd-mobile";
+import {Button, NavBar, Form, Input} from 'antd-mobile'
 
-function Login() {
+import styles from './index.module.scss'
+
+const Login = () => {
     return (
-        <div>
-            <h1>登录</h1>
-            <Button color='primary'>按钮</Button>
+        <div className={styles.root}>
+            {/*头部*/}
+            <NavBar></NavBar>
+            {/*登录表单*/}
+            <div className="login-form">
+                <h2 className="title">账号登录</h2>
+                <Form>
+                    <Form.Item className="login-item">
+                        <Input placeholder="请输入手机号"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        className="login-item"
+                        extra={<span className="code-extra">发送验证码</span>}
+                    >
+                        <Input placeholder="请输入验证码" autoComplete="off"/>
+                    </Form.Item>
+
+                    {/* noStyle 表示不提供 Form.Item 自带的样式 */}
+                    <Form.Item noStyle>
+                        <Button
+                            block
+                            type="submit"
+                            color="primary"
+                            className="login-submit"
+                        >
+                            登 录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
-    );
+    )
 }
 
-export default Login;
+export default Login
