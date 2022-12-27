e998fac4c7ce5729644b128726ca8e8b6f466b84
zhiming <zhiming.hao@qq.com>
2022-12-27 18:26:36 +0800

    token持久化

--- src/store/actions/login.ts
+++ src/store/actions/login.ts
@@ -1,11 +1,18 @@
 import type {LoginFormData, LoginResponse} from "@/types/data";
 import {RootThunkAction} from "@/types/store";
 import request from "@/utils/request";
+import {setToken} from "@/utils/auth";
 
 
 export const LoginAction = (data: LoginFormData): RootThunkAction => {
     return async (dispatch) => {
         const res: LoginResponse = await request.post('/v1_0/authorizations', data)
+        /*
+        * 持久化token:
+        * 1.本地存一份
+        * 2.redux寸一份
+        * */
+        setToken(res.data)
         dispatch({type: 'login/token', payload: res.data})
     }
 }
--- src/store/reducers/login.ts
+++ src/store/reducers/login.ts
@@ -1,10 +1,8 @@
 import {Token} from "@/types/data";
 import {LoginAction} from "@/types/store";
+import {getToken} from "@/utils/auth";
 
-const initialState: Token = {
-    token: '',
-    refresh_token: ''
-}
+const initialState: Token = getToken()
 
 export const login = (state = initialState, action: LoginAction) => {
     if (action.type === 'login/token') {
