d12a62c3b70c16be920bb387ba0a543d61205fd9
zhiming <zhiming.hao@qq.com>
2022-12-28 12:14:39 +0800

    tabbar图标修改

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,45 +1,26 @@
 import {TabBar} from 'antd-mobile'
-import {
-    AppOutline,
-    MessageOutline,
-    MessageFill,
-    UnorderedListOutline,
-    UserOutline,
-} from 'antd-mobile-icons'
+import Icon from '@/components/icon'
+import styles from './index.module.scss'
 
 // 导航栏数据
 const tabs = [
-    {
-        key: 'home',
-        title: '首页',
-        icon: <AppOutline/>,
-    },
-    {
-        key: 'todo',
-        title: '待办',
-        icon: <UnorderedListOutline/>,
-    },
-    {
-        key: 'message',
-        title: '消息',
-        // active的值是true表明被选中=》显示被选中的图标 | 值是false=》显示未被选中的图标
-        icon: (active: boolean) =>
-            active ? <MessageFill/> : <MessageOutline/>,
-    },
-    {
-        key: 'personalCenter',
-        title: '我的',
-        icon: <UserOutline/>,
-    },
+    {path: '/home/index', icon: 'iconbtn_home', text: '首页'},
+    {path: '/home/question', icon: 'iconbtn_qa', text: '问答'},
+    {path: '/home/video', icon: 'iconbtn_video', text: '视频'},
+    {path: '/home/profile', icon: 'iconbtn_mine', text: '我的'}
 ]
 
 function Layout() {
     return (
-        <div>
+        <div className={styles.root}>
             {/*导航栏*/}
-            <TabBar style={{width: '100%', position: 'fixed', bottom: 0, borderTop: '1px solid #ddd'}}>
+            <TabBar className='tab-bar'>
                 {tabs.map(item => (
-                    <TabBar.Item key={item.key} icon={item.icon} title={item.title}/>
+                    <TabBar.Item
+                        key={item.path}
+                        //icon选中的图标名字带个:_sel
+                        icon={active => (<Icon type={active ? `${item.icon}_sel` : item.icon}/>)}
+                        title={item.text}/>
                 ))}
             </TabBar>
         </div>
