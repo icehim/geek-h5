d451f705eccf55a231b57df40f864da8a6a9d499
zhiming <zhiming.hao@qq.com>
2022-12-28 18:46:55 +0800

    修改个人信息渲染到页面

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -2,88 +2,94 @@ import {Button, List, DatePicker, NavBar} from 'antd-mobile'
 import classNames from 'classnames'
 
 import styles from './index.module.scss'
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getUserEditAction} from "@/store/actions/profile";
+import {RootState} from "@/types/store";
 
 const Item = List.Item
 
 const ProfileEdit = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch<any>((getUserEditAction()))
-  }, [dispatch])
+    //1.获取修改数据
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch<any>((getUserEditAction()))
+    }, [dispatch])
 
-  return (
-      <div className={styles.root}>
-        <div className="content">
-          {/* 标题 */}
-          <NavBar
-              style={{
-                '--border-bottom': '1px solid #F0F0F0'
-              }}
-          >
-            个人信息
-          </NavBar>
+    //2.回填数据
+    const {edit} = useSelector((state: RootState) => state.profile)
+    const {photo, name, gender, birthday, intro,} = edit
 
-          <div className="wrapper">
-            {/* 列表 */}
-            <List className="profile-list">
-              {/* 列表项 */}
-              <Item
-                  extra={
-                    <span className="avatar-wrapper">
+    return (
+        <div className={styles.root}>
+            <div className="content">
+                {/* 标题 */}
+                <NavBar
+                    style={{
+                        '--border-bottom': '1px solid #F0F0F0'
+                    }}
+                >
+                    个人信息
+                </NavBar>
+
+                <div className="wrapper">
+                    {/* 列表 */}
+                    <List className="profile-list">
+                        {/* 列表项 */}
+                        <Item
+                            extra={
+                                <span className="avatar-wrapper">
                   <img
                       width={24}
                       height={24}
-                      src={'http://toutiao.itheima.net/images/user_head.jpg'}
+                      src={photo}
                       alt=""
                   />
                 </span>
-                  }
-                  arrow
-              >
-                头像
-              </Item>
-              <Item arrow extra={'黑马先锋'}>
-                昵称
-              </Item>
-              <Item
-                  arrow
-                  extra={
-                    <span className={classNames('intro', 'normal')}>
-                  {'未填写'}
+                            }
+                            arrow
+                        >
+                            头像
+                        </Item>
+                        <Item arrow extra={name}>
+                            昵称
+                        </Item>
+                        <Item
+                            arrow
+                            extra={
+                                <span className={classNames('intro', 'normal')}>
+                  {intro || '未填写'}
                 </span>
-                  }
-              >
-                简介
-              </Item>
-            </List>
+                            }
+                        >
+                            简介
+                        </Item>
+                    </List>
 
-            <List className="profile-list">
-              <Item arrow extra={'男'}>
-                性别
-              </Item>
-              <Item arrow extra={'1999-9-9'}>
-                生日
-              </Item>
-            </List>
+                    <List className="profile-list">
+                        <Item arrow extra={gender === 0 ? '男' : '女'}>
+                            性别
+                        </Item>
+                        <Item arrow extra={birthday}>
+                            生日
+                        </Item>
+                    </List>
 
-            <DatePicker
-                visible={false}
-                value={new Date()}
-                title="选择年月日"
-                min={new Date(1900, 0, 1, 0, 0, 0)}
-                max={new Date()}
-            />
-          </div>
+                    <DatePicker
+                        visible={false}
+                        value={new Date()}
+                        title="选择年月日"
+                        min={new Date(1900, 0, 1, 0, 0, 0)}
+                        max={new Date()}
+                    />
+                </div>
 
-          <div className="logout">
-            <Button className="btn">退出登录</Button>
-          </div>
+                <div className="logout">
+                    <Button className="btn">退出登录</Button>
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default ProfileEdit
