0009c337c36b272ba26789ec5a5d1ff227819d13
zhiming <zhiming.hao@qq.com>
2022-12-28 18:20:56 +0800

    渲染用户信息

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,9 +2,10 @@ import {Link, useHistory} from 'react-router-dom'
 
 import Icon from "@/components/icon";
 import styles from './index.module.scss'
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getUserAction} from "@/store/actions/profile";
+import {RootState} from "@/types/store";
 
 const Profile = () => {
     const history = useHistory()
@@ -14,6 +15,14 @@ const Profile = () => {
         dispatch<any>(getUserAction())
     }, [dispatch])
 
+    //2.从redux获取登录人信息
+    //泛型变量指定类型
+    // const user = useSelector<RootState, RootState['profile']>(state => state.profile)
+    //函数参数指定类型(推荐)
+    const {user} = useSelector((state: RootState) => state.profile)
+
+    const {photo, name, like_count, follow_count, fans_count, art_count} = user
+
     return (
         <div className={styles.root}>
             <div className="profile">
@@ -21,11 +30,11 @@ const Profile = () => {
                 <div className="user-info">
                     <div className="avatar">
                         <img
-                            src={'http://toutiao.itheima.net/images/user_head.jpg'}
+                            src={photo}
                             alt=""
                         />
                     </div>
-                    <div className="user-name">黑马先锋</div>
+                    <div className="user-name">{name}</div>
                     {/*修改个人信息跳转*/}
                     <Link to="/profile/edit">
                         个人信息 <Icon type="iconbtn_right"/>
@@ -43,19 +52,19 @@ const Profile = () => {
                 {/* 动态 - 对应的这一行 */}
                 <div className="count-list">
                     <div className="count-item">
-                        <p>1</p>
+                        <p>{art_count}</p>
                         <p>动态</p>
                     </div>
                     <div className="count-item">
-                        <p>9</p>
+                        <p>{follow_count}</p>
                         <p>关注</p>
                     </div>
                     <div className="count-item">
-                        <p>99</p>
+                        <p>{fans_count}</p>
                         <p>粉丝</p>
                     </div>
                     <div className="count-item">
-                        <p>200</p>
+                        <p>{like_count}</p>
                         <p>被赞</p>
                     </div>
                 </div>
