e692b04a0bd0bdfc026dcdf4d53435bf36d87cd4
zhiming <zhiming.hao@qq.com>
2022-12-28 11:47:02 +0800

    首页TabBar布局

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,7 +1,47 @@
+import {TabBar} from 'antd-mobile'
+import {
+    AppOutline,
+    MessageOutline,
+    MessageFill,
+    UnorderedListOutline,
+    UserOutline,
+} from 'antd-mobile-icons'
+
+// 导航栏数据
+const tabs = [
+    {
+        key: 'home',
+        title: '首页',
+        icon: <AppOutline/>,
+    },
+    {
+        key: 'todo',
+        title: '待办',
+        icon: <UnorderedListOutline/>,
+    },
+    {
+        key: 'message',
+        title: '消息',
+        // active的值是true表明被选中=》显示被选中的图标 | 值是false=》显示未被选中的图标
+        icon: (active: boolean) =>
+            active ? <MessageFill/> : <MessageOutline/>,
+    },
+    {
+        key: 'personalCenter',
+        title: '我的',
+        icon: <UserOutline/>,
+    },
+]
+
 function Layout() {
     return (
         <div>
-            <h1>首页</h1>
+            {/*导航栏*/}
+            <TabBar style={{width: '100%', position: 'fixed', bottom: 0, borderTop: '1px solid #ddd'}}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title}/>
+                ))}
+            </TabBar>
         </div>
     );
 }
