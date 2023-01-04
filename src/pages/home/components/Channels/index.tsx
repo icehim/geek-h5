b91b49b8c5e1c8c0a06e66a86ab872557b1c9842
zhiming <zhiming.hao@qq.com>
2023-01-04 19:56:48 +0800

    我的频道数据

--- src/pages/home/components/Channels/index.tsx
+++ src/pages/home/components/Channels/index.tsx
@@ -2,16 +2,22 @@ import classnames from 'classnames'
 
 import Icon from '@/components/icon'
 import styles from './index.module.scss'
+import {useSelector} from "react-redux";
+import {RootState} from "@/types/store";
 
 type Props = {
     onClose: () => void
 }
 const Channels = ({onClose}: Props) => {
+    //1.获取我的频道数据
+    const {userChannel} = useSelector((state: RootState) => state.home)
     return (
         <div className={styles.root}>
+            {/*头部*/}
             <div className="channel-header">
                 <Icon onClick={onClose} type="iconbtn_channel_close"/>
             </div>
+            {/*内容*/}
             <div className="channel-content">
                 {/* 编辑时，添加类名 edit */}
                 <div className={classnames('channel-item')}>
@@ -20,12 +26,18 @@ const Channels = ({onClose}: Props) => {
                         <span className="channel-item-title-extra">点击进入频道</span>
                         <span className="channel-item-edit">编辑</span>
                     </div>
+                    {/*1.我的频道列表数据*/}
                     <div className="channel-list">
                         {/* 选中时，添加类名 selected */}
-                        <span className={classnames('channel-list-item')}>
-              推荐
-              <Icon type="iconbtn_tag_close"/>
-            </span>
+
+                        {
+                            userChannel.map(item => (
+                                <span key={item.id} className={classnames('channel-list-item')}>
+                                    {item.name}
+                                    <Icon type="iconbtn_tag_close"/>
+                                </span>
+                            ))
+                        }
                     </div>
                 </div>
 
@@ -34,6 +46,7 @@ const Channels = ({onClose}: Props) => {
                         <span className="channel-item-title">频道推荐</span>
                         <span className="channel-item-title-extra">点击添加频道</span>
                     </div>
+                    {/*2.可选频道列表数据*/}
                     <div className="channel-list">
                         <span className="channel-list-item">+ HTML</span>
                     </div>
