f99a53f6aad450d61f4dfad9381ea7df33853845
zhiming <zhiming.hao@qq.com>
2023-01-04 18:16:32 +0800

    首页频道tabs组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,36 @@
-function Home() {
+import styles from './index.module.scss'
+import Icon from "@/components/icon";
+import {Tabs} from "antd-mobile";
+
+const Home = () => {
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
+        <div className={styles.root}>
+            {/* 频道 Tabs 列表 */}
+
+            <Tabs className="tabs" activeLineMode="fixed">
+                <Tabs.Tab title="推荐" key="1">
+                    推荐频道的内容
+                </Tabs.Tab>
+                <Tabs.Tab title="html" key="2">
+                    html频道的内容
+                </Tabs.Tab>
+                <Tabs.Tab title="开发者资讯" key="3">
+                    开发者资讯频道的内容
+                </Tabs.Tab>
+                <Tabs.Tab title="c++" key="4">
+                    c++频道的内容
+                </Tabs.Tab>
+                <Tabs.Tab title="css" key="5">
+                    css频道的内容
+                </Tabs.Tab>
+            </Tabs>
+
+            <div className="tabs-opration">
+                <Icon type="iconbtn_search"/>
+                <Icon type="iconbtn_channel"/>
+            </div>
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Home
