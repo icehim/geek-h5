cfadd9fd63deaf66b04959badff444bc5429b4c1
zhiming <zhiming.hao@qq.com>
2023-01-05 19:46:11 +0800

    搜索结果下拉加载更多

--- src/pages/Search/Result/index.tsx
+++ src/pages/Search/Result/index.tsx
@@ -1,27 +1,46 @@
 import {useHistory} from 'react-router-dom'
-import {NavBar} from 'antd-mobile'
+import {InfiniteScroll, NavBar} from 'antd-mobile'
 
 import ArticleItem from '@/components/ArticleItem'
 
 import styles from './index.module.scss'
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {getSearchList} from "@/api/search";
 import {ArticlesItem} from "@/types/data";
 
 const Result = () => {
     const history = useHistory()
+    // 获取搜索的query参数
+    const query = new URLSearchParams(window.location.search)
+    const q = query.get('q') || ''
+
     //结果列表
     const [list, setList] = useState<ArticlesItem[]>([])
 
     useEffect(() => {
         const sea = new URLSearchParams(window.location.search)
-        console.log(sea.get('q'))
         const getResult = async () => {
             const {data: {results}} = await getSearchList({page: 1, q: sea.get('q') || ''})
             setList(results)
         }
         getResult()
     }, [])
+
+    const [hasMore, setHasMore] = useState(true)
+    const page = useRef(1)
+
+    async function loadMore() {
+        const {
+            data: {results, total_count},
+        } = await getSearchList({q, page: page.current})
+        setList([...list, ...results])
+        if (total_count === list.length) {
+            setHasMore(false)
+        } else {
+            page.current++
+        }
+    }
+
     //渲染结果列表
     const renderArticleList = () => {
         return list.map((item) => {
@@ -39,7 +58,9 @@ const Result = () => {
     return (
         <div className={styles.root}>
             <NavBar onBack={() => history.go(-1)}>搜索结果</NavBar>
-            <div className="article-list">{renderArticleList()}</div>
+            <div className="article-list">{renderArticleList()}
+                <InfiniteScroll loadMore={loadMore} hasMore={hasMore}/>
+            </div>
         </div>
     )
 }
