437f0cfebe08238920a07150659c598738ed8b39
zhiming <zhiming.hao@qq.com>
2023-01-05 16:42:47 +0800

    渲染联想词

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -6,25 +6,36 @@ import Icon from '@/components/icon'
 import styles from './index.module.scss'
 import {useState} from "react";
 import {getSuggestsListApi} from "@/api/search";
-import {debounce} from "lodash";
+// import {debounce} from "lodash";
+import {useDebounceFn} from "ahooks";
 
 //1.使用lodash进行联想词防抖
 // debounceFN是经过防抖处理的可执行函数
-const debounceFN = debounce(async (value: string) => {
-    const {data: {options}} = await getSuggestsListApi(value)
-    console.log(options)
-}, 600)
+// const debounceFN = debounce(async (value: string) => {
+//     const {data: {options}} = await getSuggestsListApi(value)
+//     console.log(options)
+// }, 600)
 
 const SearchPage = () => {
     const history = useHistory()
     //1.搜索输入受控=》获取用户输入
+    //搜索关键词
     const [keyWord, setKeyWord] = useState('')
+    //联想词列表
+    const [suggests, setSuggests] = useState<string[]>([])
+
+    //2.使用useDebounceFn处理防抖
+    const {run} = useDebounceFn(async (value: string) => {
+        const {data: {options}} = await getSuggestsListApi(value)
+        setSuggests(options)
+    }, {wait: 600})
     const changeWord = async (value: string) => {
         setKeyWord(value)
         //2.获取联想词数据
         //注意:判断是否为空，空不处理
         if (!value.trim()) return
-        debounceFN(value)
+        // debounceFN(value) //使用lodash
+        run(value)//使用 ahooks
     }
     return (
         <div className={styles.root}>
@@ -60,14 +71,19 @@ const SearchPage = () => {
                 </div>
             )}
             {/*搜索联想词*/}
-            <div className={classnames('search-result', true ? 'show' : '')}>
-                <div className="result-item">
-                    <Icon className="icon-search" type="iconbtn_search"/>
-                    <div className="result-value text-overflow">
-                        <span>黑马</span>
-                        程序员
-                    </div>
-                </div>
+            <div className={classnames('search-result', suggests.length > 0 && 'show')}>
+
+                {
+                    suggests.map(item => (
+                        item && (<div key={item} className="result-item">
+                            <Icon className="icon-search" type="iconbtn_search"/>
+                            <div className="result-value text-overflow">
+                                {/*<span>黑马</span>*/}
+                                {item}
+                            </div>
+                        </div>)
+                    ))
+                }
             </div>
         </div>
     )
